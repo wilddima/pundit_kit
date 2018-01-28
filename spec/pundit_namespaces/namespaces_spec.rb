0d817c0b57fbbc4e10778fc874a5828640abc1b8
WildDima <dtopornin@gmail.com>
2018-01-28 16:41:53 +0300

    Add first spec

---
+++ spec/pundit_namespaces/namespaces_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+RSpec.describe PunditNamespaces::Namespaces do
+  #let(:namespaces) { described_class.new }
+  #let(:namespaces_block) do
+  #  lambda do
+  #    namespace :first, if: -> (x) { x > 0 } do
+  #      root_namespace
+  #      namespace :one, if: -> (x) { x == 1 }
+  #      namespace :two, if: -> (x) { x == 2 }
+  #    end
+
+  #    namespace :second, if: -> (x) { x < 0 } do
+  #      root_namespace
+  #      namespace :one, if: -> (x) { x == -1 }
+  #      namespace :two, if: -> (x) { x == -2 }
+  #    end
+  #  end
+  #end
+
+  #before do
+  #  namespaces.instance_eval(&namespaces_block)
+  #end
+
+  let(:namespaces) do
+    PunditNamespaces.routes do
+      namespace :first, if: -> (x) { x > 0 } do
+        root_namespace
+        namespace :one, if: -> (x) { x == 1 }
+        namespace :two, if: -> (x) { x == 2 }
+      end
+
+      namespace :second, if: -> (x) { x < 0 } do
+        root_namespace
+        namespace :one, if: -> (x) { x == -1 }
+        namespace :two, if: -> (x) { x == -2 }
+      end
+    end
+  end
+
+  describe '#matches' do
+    subject { namespaces.matches(matcher) }
+
+    let(:matcher) { 1 }
+
+    it '' do
+      expect(subject).to eq([:first, [:_root_namespace, :one]])
+    end
+  end
+end
--- spec/pundit_namespaces_spec.rb
+++ spec/pundit_namespaces_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe PunditNamespaces do
   it "has a version number" do
     expect(PunditNamespaces::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
