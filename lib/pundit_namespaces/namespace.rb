d3a48f8380a751c6ed1ee38ebdf9bb7c6d123c8e
WildDima <dtopornin@gmail.com>
2018-02-25 23:23:24 +0300

    Add presence and error property to namespace

--- lib/pundit_namespaces/namespace.rb
+++ lib/pundit_namespaces/namespace.rb
@@ -1,11 +1,12 @@
 module PunditNamespaces
   class Namespace
-    attr_reader :name, :options, :match, :presence
+    attr_reader :name, :options, :match, :presence, :error
 
     def initialize(name, options = {})
       @name = name
       @match = options.delete(:if) || ->(_) { true }
       @presence = options[:presence].nil? ? true : options.delete(:presence)
+      @error = options.delete(:error) || Pundit::NotAuthorizedError
     end
 
     def match?(*args)
--- lib/pundit_namespaces/policy_finder.rb
+++ lib/pundit_namespaces/policy_finder.rb
@@ -9,12 +9,26 @@ module PunditNamespaces
 
     def all_policies_for(obj)
       PunditNamespaces.namespaces.matches(matcher).map do |namespace|
-        next policy_finder(namespace, obj).policy! if namespace.map(&:presence?).all?
-        policy_finder(namespace, obj).policy
+        policy = policy_finder(namespace, obj).policy! if namespace.map(&:presence?).all?
+        next policy_obj(policy, namespace) if policy
+        policy = policy_finder(namespace, obj).policy
+        policy_obj(policy, namespace)
       end.compact
     end
 
     private
+
+    def policy_obj(policy, namespace)
+      return unless policy
+      { policy: policy,
+        error: find_error(namespace).error }
+    end
+
+    def find_error(namespace)
+      namespace
+        .reverse
+        .find { |ns| ns != Pundit::NotAuthorizedError } || Pundit::NotAuthorizedError
+    end
 
     def policy_finder(namespace, obj)
       ns = namespace.map(&:name).delete_if { |n| n == :_root_namespace }
