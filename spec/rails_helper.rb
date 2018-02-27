12d17d637519377c7d328adf17f317464e2da26a
WildDima <dtopornin@gmail.com>
2018-02-27 14:44:44 +0300

    Add helper to dummy rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,3 +5,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../dummy/config/environment.rb', __FILE__)
 require 'spec_helper'
 require 'rspec/rails'
+
+Dir['./spec/pundit_namespaces/dummy/support/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.include CurrentUserHelper, type: :controller
+end
