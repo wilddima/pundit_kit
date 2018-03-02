76271f01066d619272da7611741b7d18795cc528
WildDima <dtopornin@gmail.com>
2018-03-02 19:55:56 +0300

    Include PunditNamespaces and add pundit namespaces matcher to application controller

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController <  ActionController::Base
   include Pundit
+  include PunditNamespaces
 
   def current_user
     @current_user
+  end
+
+  def pundit_namespace_matcher
+    pundit_user
   end
 end
