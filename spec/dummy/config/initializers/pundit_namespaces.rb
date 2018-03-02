8dd69d88c8df2f10bbd2bc98e9e06856417a57f7
WildDima <dtopornin@gmail.com>
2018-03-02 19:58:53 +0300

    Add pundit namespace configuration

--- spec/dummy/config/initializers/pundit_namespaces.rb
+++ spec/dummy/config/initializers/pundit_namespaces.rb
@@ -1,8 +1,11 @@
+class ClientNotAllowedError < StandardError; end
+class UserNotAllowedError < StandardError; end
+
 PunditNamespaces.routes do
   namespace :staff, if: -> (user) { user.staff? } do
     namespace :admin, if: -> (user) { user.admin? }
-    namespace :user, if: -> (user) { user.user? }
+    namespace :user, if: -> (user) { user.user? }, error: UserNotAllowedError
   end
 
-  namespace :client, if: -> (user) { user.client? }
+  namespace :client, if: -> (user) { user.client? }, error: ClientNotAllowedError
 end
