9508f0e21098cc352af8cad5baa2b3932145dd80
WildDima <dtopornin@gmail.com>
2018-03-02 19:56:52 +0300

    Add controllers for dummy app

--- spec/dummy/app/controllers/posts_controller.rb
+++ spec/dummy/app/controllers/posts_controller.rb
@@ -1,4 +1,27 @@
 class PostsController < ApplicationController
-  def show; end
+  def index
+    authorize_all :post
+    render plain: 'Ok'
+  end
+
+  def show
+    authorize_all :post
+    render plain: 'Ok'
+  end
+
+  def create
+    authorize_all :post
+    render plain: 'Ok'
+  end
+
+  def update
+    authorize_all :post
+    render plain: 'Ok'
+  end
+
+  def destroy
+    authorize_all :post
+    render plain: 'Ok'
+  end
 end
 
